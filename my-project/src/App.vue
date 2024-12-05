9ad0175a279e98597df7560534337ffcc50409d0
Fantarina <49835289+Fantarina@users.noreply.github.com>
2024-12-06 00:21:46 +0400

    Bisous

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -15,10 +15,70 @@
         >
         </CirclePoint>
       </div>
-      <div class="section right-section">
+      <div id="Poumonshomme" class="Poumons">
         <TextCard
-          title="Points Ronds"
-          content="Les points ronds permettent de représenter des indicateurs ou des éléments visuels interactifs."
+          title="Poumons Humains"
+          content="Les principales fonctions des poumons sont de transférer dans le sang l'oxygène présent dans l'air et d'évacuer dans l'air le dioxyde de carbone présent dans le sang.."
+        />
+      </div>
+      <div id="Poumonsoceans" class="Poumons">
+        <TextCard
+          title="Poumons Océans"
+          content="Ils servent de système respiratoire de la Terre, produisant de l’oxygène pour la vie et absorbant le dioxyde de carbone et les déchets. Les océans stockent et absorbent le dioxyde de carbone mondial, tandis que le phytoplancton marin génère l’oxygène nécessaire à la survie."
+        />
+      </div>
+      <div id="CirculationHumain" class="Circulation">
+        <TextCard
+          title="Appareil circulatoire humain"
+          content="Les rôles du système circulatoire sont les suivants. Transporter les gaz respiratoires, les nutriments et les déchets. Participer aux échanges gazeux, aux échanges de nutriments et de déchets entre les cellules et le sang."
+        />
+      </div>
+      <div id="CirculationOcean" class="Circulation">
+        <TextCard
+          title="Circulation océanique"
+          content="Les courants marins régulent la chaleur des différents continents ainsi que l’humidité de l’air. La Terre n’étant pas ensoleillée de manière égale, les courants vont redistribuer les énergies solaires des zones chaudes aux zones froides. Par exemple, le courant marin le plus connu, le Gulf Stream, transporte la chaleur des tropiques pour réchauffer l’Europe."
+        />
+      </div>
+      <div id="RegThermHomme" class="Regulation">
+        <TextCard
+          title="Régulation thermique chez l'homme"
+          content="Le corps maintient une température constante grâce à des mécanismes comme la transpiration ou les frissons."
+        />
+      </div>
+      <div id="RegThermHomme" class="Regulation">
+        <TextCard
+          title="Régulation thermique chez l'homme"
+          content="Le corps maintient une température constante grâce à des mécanismes comme la transpiration ou les frissons."
+        />
+      </div>
+      <div id="RegThermOcean" class="Regulation">
+        <TextCard
+          title="Régulation thermique par l'océan"
+          content="L'océan agit comme un régulateur thermique global, absorbant et redistribuant la chaleur solaire pour stabiliser les températures planétaires."
+        />
+      </div>
+      <div id="EcoHomme" class="Ecosysteme">
+        <TextCard
+          title="Microbiome humain"
+          content="Le microbiome humain abrite des milliards de micro-organismes dans le tube digestif, sur la peau, et dans d'autres parties du corps, jouant un rôle crucial pour la santé."
+        />
+      </div>
+      <div id="EcoOcean" class="Ecosysteme">
+        <TextCard
+          title="Ecosysteme Marin"
+          content="L'océan est un écosystème complexe et varié, abritant une diversité impressionnante d'espèces, des microorganismes aux grandes créatures marines."
+        />
+      </div>
+      <div id="MemoireHumain" class="Memoire">
+        <TextCard
+          title="Mémoire humaine"
+          content="Le cerveau humain stocke des souvenirs et contrôle des comportements à travers des connexions neurologiques."
+        />
+      </div>
+      <div id="MemoireOcean" class="Memoire">
+        <TextCard
+          title="Mémoire de l'Océan"
+          content="Les sédiments marins et les formations géologiques enregistrent l’histoire de la Terre, y compris les changements climatiques et les activités tectoniques."
         />
       </div>
     </main>
