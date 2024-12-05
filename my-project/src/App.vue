97ddb53e0b4b48786f72ea0db47a39fd0789fee2
unknown <davidra.andriamisaina@esiroi.re>
2024-12-06 01:47:55 +0400

    Changement source

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -18,8 +18,16 @@
       <div v-if="visibleCards['Poumons']" id="Poumonsoceans" class="Poumons">
         <TextCard
           title="Poumons Océans"
-          content="Ils servent de système respiratoire de la Terre, produisant de l’oxygène pour la vie et absorbant le dioxyde de carbone et les déchets."
-          source="https://www.raceforwater.org/fr/"
+          content="Les principales fonctions des poumons sont de transférer dans le sang l'oxygène présent dans l'air et d'évacuer dans l'air le dioxyde de carbone présent dans le sang"
+          source="https://www.portfrejus.fr/poumon-bleu-de-la-planete/"
+        />
+      </div>
+
+      <div v-if="visibleCards['Poumons']" id="Poumonhumains" class="Poumons">
+        <TextCard
+          title="Poumons Océans"
+          content="Les principales fonctions des poumons sont de transférer dans le sang l'oxygène présent dans l'air et d'évacuer dans l'air le dioxyde de carbone présent dans le sang"
+          source="https://cancer.ca/fr/cancer-information/cancer-types/lung/what-is-lung-cancer."
         />
       </div>
 
@@ -27,14 +35,14 @@
         <TextCard
           title="Appareil circulatoire humain"
           content="Les rôles du système circulatoire sont les suivants. Transporter les gaz respiratoires, les nutriments et les déchets. Participer aux échanges gazeux, aux échanges de nutriments et de déchets entre les cellules et le sang."
-          source="https://www.raceforwater.org/fr/"
+          source="https://www.alloprof.qc.ca/fr/eleves/bv/sciences/le-systeme-circulatoire-et-son-anatomie"
         />
       </div>
       <div v-if="visibleCards['Circulation']" id="CirculationOcean" class="Circulation">
         <TextCard
           title="Circulation océanique"
           content="Les courants marins régulent la chaleur des différents continents ainsi que l’humidité de l’air."
-          source="https://www.raceforwater.org/fr/"
+          source="https://www.oceanclock.com/fr/blog/13-les-secrets-des-courants-marins"
         />
       </div>
 
@@ -42,14 +50,14 @@
         <TextCard
           title="Régulation thermique chez l'homme"
           content="Le corps maintient une température constante grâce à des mécanismes comme la transpiration ou les frissons."
-          source="https://www.raceforwater.org/fr/"
+          source="https://www.sciencesetavenir.fr/sante/comment-fonctionne-la-thermoregulation-chez-l-etre-humain_165495"
         />
       </div>
       <div v-if="visibleCards['Regulation']" id="RegThermOcean" class="Regulation">
         <TextCard
           title="Régulation thermique par l'océan"
           content="L'océan agit comme un régulateur thermique global, absorbant et redistribuant la chaleur solaire pour stabiliser les températures planétaires."
-          source="https://www.raceforwater.org/fr/"
+          source="https://www.afd.fr/fr/actualites/locean-regulateur-du-climat-et-foyer-de-la-biodiversite"
         />
       </div>
 
@@ -57,14 +65,14 @@
         <TextCard
           title="Microbiome humain"
           content="Le microbiome humain abrite des milliards de micro-organismes dans le tube digestif, sur la peau, et dans d'autres parties du corps, jouant un rôle crucial pour la santé."
-          source="https://www.raceforwater.org/fr/"
+          source="https://www.inrae.fr/alimentation-sante-globale/microbiote_intestinal"
         />
       </div>
       <div v-if="visibleCards['Ecosysteme']" id="EcoOcean" class="Ecosysteme">
         <TextCard
           title="Ecosystème Marin"
           content="L'océan est un écosystème complexe et varié, abritant une diversité impressionnante d'espèces, des microorganismes aux grandes créatures marines."
-          source="https://www.raceforwater.org/fr/"
+          source="https://planet-vie.ens.fr/thematiques/ecologie/biodiversite/la-biodiversite-dans-l-ocean"
         />
       </div>
 
@@ -72,14 +80,14 @@
         <TextCard
           title="Mémoire humaine"
           content="Le cerveau humain stocke des souvenirs et contrôle des comportements à travers des connexions neurologiques."
-          source="https://www.raceforwater.org/fr/"
+          source="https://www.science-et-vie.com/cerveau-et-intelligence/souvenirs-cerveau-memoire-cellules-engrammes-neurones-119783.html"
         />
       </div>
       <div v-if="visibleCards['Memoire']" id="MemoireOcean" class="Memoire">
         <TextCard
           title="Mémoire de l'Océan"
           content="Les sédiments marins et les formations géologiques enregistrent l’histoire de la Terre, y compris les changements climatiques et les activités tectoniques."
-          source="https://www.raceforwater.org/fr/"
+          source="https://www.futura-sciences.com/planete/questions-reponses/geologie-retrace-t-on-histoire-tectonique-grace-sediments-15518/"
         />
       </div>
     </main>
